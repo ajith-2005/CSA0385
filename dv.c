1672296e9f36fac87b6b397f5ca0f9f7d45fb6c4
ajith-2005 <ajithchoppadandi@gmail.com>
2024-07-27 13:21:55 +0530

    Add files via upload

---
+++ dv.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+	int a,b,c,sum;
+	printf("Enter a number:");
+	scanf("%d %d %d",&a,&b,&c);
+	sum=a+b+c;
+	printf("The sum 0f three of numbers is: 6",a,b,c,sum);
+	return 0;
+	
+}
